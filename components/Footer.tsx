90356eb69a74873fb7491650207f27e45f8ad02d
mohammadne <mohammadne.dev@gmail.com>
2023-04-09 12:49:12 +0330

    feat: add components

---
+++ components/Footer.tsx
@@ -0,0 +1,33 @@
+type Props = {
+  noPadding?: boolean;
+};
+
+const Footer: React.FC<Props> = ({ noPadding = false }) => {
+  return (
+    <footer
+      className={`${noPadding ? "pb-4" : "pb-24"} md:pb-4 text-center mt-auto`}
+    >
+      <div>
+        Coded with <span className="sr-only">love</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 mx-1 inline-block mb-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>{" "}
+        by Mohammad Nasr
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
