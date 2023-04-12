358f4d2155a74c05e47219116ff15205897e2d0b
mohammadne <mohammadne.dev@gmail.com>
2023-04-12 22:58:32 +0330

    feat: change structure of the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./sections/**/*.{js,ts,jsx,tsx}",
+    "src/pages/**/*.{js,ts,jsx,tsx}",
+    "src/components/**/*.{js,ts,jsx,tsx}",
+    "src/sections/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
